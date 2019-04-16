bc75a664dd3c8b662b54ad1a42270dc5e3a71fd0
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2019-04-16 09:48:45 -0300

    Script's vídeos R-03

---
+++ R - Avançado - Data Mining/Script_R_Avancado.R
@@ -0,0 +1,36 @@
+#####################################################################################
+# Aula 01 - Introdu??o
+# Conte?do:
+
+# 1. Regress?o:
+# 1.1. Regress?o linear (simples e m?ltipla)
+# 1.2. Regress?o polinomial
+# 1.3. Regress?o com ?rvores de decis?o e random forest
+# 1.4. Regress?o com vetores de suporte
+# 1.5. Regress?o com redes neurais artificiais
+
+# 2. Regras de Associa??o:
+# 2.1. Apriori
+# 2.2. ECLAT
+
+# 3. Classifica??o:
+# 3.1. Neive Bays
+# 3.2. ?rvores de Decis?o - Rpart
+# 3.3. Regras - (PRISM, OneR, CN2)
+# 3.4. Aprendizagem Baseado em Inst?ncias - KNN
+# 3.5. M?quina de Vetores de Suporte - SVM
+# 3.6. Regress?o Logist?ca
+# 3.7. RNA
+
+# 4. Agrupamentos
+# 4.1. K-means
+# 4.2. Fuzzy c-means
+# 4.3. K-medoids
+# 4.4. DBSCAN
+# 4.5. Hierárquico
+
+# 5. Séries Temporais
+
+# 6. Mineração de Textos
+
+# 7. Redes Sociais e Grafos
