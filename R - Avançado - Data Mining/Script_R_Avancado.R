fc6b50c6331738ffb0614da7c509b1dae62a13f9
Prof-Rodrigo-Silva <profrodrigorosadasilva@gmail.com>
2019-07-28 18:20:38 -0300

    OneR

--- R - Avançado - Data Mining/Script_R_Avancado.R
+++ R - Avançado - Data Mining/Script_R_Avancado.R
@@ -634,8 +634,51 @@ plot(classificador,type="simple")
 #
 
 ##########################################################################################
-# 3. Classificassão
-# 3.3. Regras - (PRISM, OneR, CN2)
+# 3. Classificassão - Regras
+# 3.3.1. OneR
+
+library(readr)
+cogumelos = read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data")
+
+colnames(cogumelos)=c('Class','cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment', 
+                  'gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring',
+                  'stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type',
+                  'veil-color','ring-number','ring-type','spore-print-color','population','habitat')
+
+cogumelos1 = cogumelos
+
+##########################################################################################
+# 3. Classificassão - Regras
+# 3.3.2. PRISM
+
+library(readr)
+cogumelos = read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data")
+
+colnames(cogumelos)=c('Class','cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment', 
+                      'gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring',
+                      'stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type',
+                      'veil-color','ring-number','ring-type','spore-print-color','population','habitat')
+
+cogumelos1 = cogumelos
+
+library(readr)
+vinho = read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data")
+
+
+
+##########################################################################################
+# 3. Classificassão - Regras 
+# 3.3.3. CN2
+
+library(readr)
+cogumelos = read_csv("https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.data")
+
+colnames(cogumelos)=c('Class','cap-shape','cap-surface','cap-color','bruises','odor','gill-attachment', 
+                      'gill-spacing','gill-size','gill-color','stalk-shape','stalk-root','stalk-surface-above-ring',
+                      'stalk-surface-below-ring','stalk-color-above-ring','stalk-color-below-ring','veil-type',
+                      'veil-color','ring-number','ring-type','spore-print-color','population','habitat')
+
+cogumelos1 = cogumelos
 
 ##########################################################################################
 # 3. Classificass?o
