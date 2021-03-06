f541debc3a09063c91a5fa0de015070a81e30c09
Rodrigo Silva <33011697+Prof-Rodrigo-Silva@users.noreply.github.com>
2021-03-06 19:56:28 -0300

    Removendo chave aleatória

---
+++ mapas_R.R
@@ -0,0 +1,30 @@
+install.packages("devtools")
+install.packages("htmltools")
+install.packages("httpuv")
+install.packages("mime", dependencies=T)
+install.packages("xtable", dependencies=T)
+install.packages("magrittr", dependencies=T)
+install.packages("crosstalk", dependencies=T)
+install.packages("jsonlite", dependencies=T)
+install.packages("yaml", dependencies=T)
+install.packages("shiny", dependencies=T) # explorar mais
+install.packages("leaflet", dependencies=T)
+install.packages("Rcpp", dependencies=T)
+install.packages("ggplot2",dependencies = T)
+install.packages("ggmap",dependencies = T)
+
+
+install.packages("rgdal",dependencies = T)
+library(rgdal)
+
+
+library(ggplot2)
+library(maps)
+library(ggmap)
+library(magrittr)
+library(leaflet)
+library(jsonlite)
+
+shape = 
+
+register_google(key = "")
